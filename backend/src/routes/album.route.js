f4bc28fb6388fde8de8e89944a6bd8621789eeb5
Zeplin Rowe <rowe_michael101@yahoo.com>
2025-10-09 20:31:47 -0400

    Album routes

--- backend/src/routes/album.route.js
+++ backend/src/routes/album.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { getAllAlbums, getAlbumById } from "../controller/album.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Album route with GET method");
-});
+router.get("/", getAllAlbums);
+
+router.get("/:albumId", getAlbumById);
 
 export default router;
