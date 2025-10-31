be2d03c7e20fb8d9b44cf8e487120e8ba37aaa85
Zeplin Rowe <rowe_michael101@yahoo.com>
2025-10-31 17:29:08 -0400

    Deploy V5

--- backend/src/index.js
+++ backend/src/index.js
@@ -74,7 +74,7 @@ app.use("/api/stats", statRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.all("*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
   });
 }
